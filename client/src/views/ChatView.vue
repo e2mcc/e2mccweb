d43190795c44d1aa210e4163528b510dacfa2ab6
e2mcc <e2mcc@foxmail.com>
2023-05-03 00:19:19 +0800

    更新

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -1,8 +1,31 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<!--这是一个容器（container），用来包裹导航栏和内容。-->
-		<div class="container">
-			<router-link class="navbar-brand" href="#" to="/">并行计算实验室</router-link>
-		</div>
-	</nav>
+	<div>
+		<form @submit.prevent="login">
+			<label for="email">Email:</label>
+			<input type="email" id="email" v-model="email" />
+
+			<label for="password">Password:</label>
+			<input type="password" id="password" v-model="password" />
+
+			<button type="submit">Login</button>
+		</form>
+	</div>
 </template>
+  
+<script>
+export default {
+	data() {
+		return {
+			email: "",
+			password: "",
+		};
+	},
+	
+	methods: {
+		async login() {
+			console.log("hahaha");
+		},
+	},
+};
+</script>
+  
--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,45 +1,89 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<!--这是一个容器（container），用来包裹导航栏和内容。-->
-		<div class="container">
-			<router-link class="navbar-brand" href="#" to="/">并行计算实验室</router-link>
-			<div class="collapse navbar-collapse" id="navbarResponsive">
-				<ul class="navbar-nav ml-auto">
-					<!-- v-if="!$store.getters.isAuthenticated"> -->
-					<!-- 这里应该有一些菜单项 -->
-					<li class = "nav-link">
-						<!-- 这是一个路由链接（router-link），用来跳转到/login -->
-						<router-link to="/login" class="nav-link">登录</router-link>
-					</li>
-					<li class = "nav-link">
-						<!-- 这是一个路由链接（router-link），用来跳转到/register -->
-						<router-link to="/register" class="nav-link">注册</router-link>
-					</li>
-				</ul>
+	<div>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<!--这是一个容器（container），用来包裹导航栏和内容。-->
+			<div class="container">
+				<router-link class="navbar-brand" href="#" to="/">并行计算实验室</router-link>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto">
+						<!-- v-if="!$store.getters.isAuthenticated"> -->
+						<!-- 这里应该有一些菜单项 -->
+						<li class = "nav-link">
+							<!-- 这是一个路由链接（router-link），用来跳转到/login -->
+							<router-link to="/login" class="nav-link">登录</router-link>
+						</li>
+						<li class = "nav-link">
+							<!-- 这是一个路由链接（router-link），用来跳转到/register -->
+							<router-link to="/register" class="nav-link">注册</router-link>
+						</li>
+					</ul>
+				</div>
 			</div>
-		</div>
-	</nav>
-	<div class="col-4 offset-4 mt-3">
-		<div class="mb-3">
-			<label for="email" class="form-label">登录邮箱地址</label>
-			<input type="email" class="form-control" id="email" placeholder="name@hnu.edu.cn">
-		</div>
-		<div class="mb-3">
-			<label for="password" class="form-label">登录密码</label>
-			<input type="password" class="form-control" id="password" v-model="password">
-		</div>
-		<div class="mb-3">
-			<button class="btn btn-primary" @click.prevent="onSubmit">登录</button>
-		</div>
-		<div class="text-center">
-			我没有账户： <router-link to="/register">马上注册</router-link>
+		</nav>
+		<div class="col-4 offset-4 mt-3">
+			<form @submit.prevent="onSubmit">
+				<div class="mb-3">
+					<label for="email" class="form-label">登录邮箱地址</label>
+					<input type="email" class="form-control" id="email" placeholder="name@hnu.edu.cn" v-model="user.email">
+				</div>
+				<div class="mb-3">
+					<label for="password" class="form-label">登录密码</label>
+					<input type="password" class="form-control" id="password" v-model="user.password">
+				</div>
+				<div class="mb-3">
+					<button class="btn btn-primary" type="submit">登录</button>
+				</div>
+				<div class="text-center">
+					我没有账户： <router-link to="/register">马上注册</router-link>
+				</div>
+			</form>
 		</div>
 	</div>
 </template>
 
 
 <script>
+import axios from 'axios';
 export default {
 	name: 'LoginView',
-  }
+
+	data() {
+		return {
+			user: {
+				email: '',
+				password: '',
+			},
+		};
+	},
+
+	methods: {
+		async onSubmit() {
+			console.log('123');
+			
+			const api = axios.create({
+				baseURL: 'http://localhost:3000',
+				withCredentials: true, // 允许发送跨域请求
+			});
+
+			try {
+				const response = await api.post('/api/login', this.user);
+				// 判断axios.post返回的 json 的内容
+				// if ('message' in response.data){
+				// 	console.log(response.data.message);
+				// }else if('token' in response.data) {
+				// 	// 获得 token
+				// 	const token = response.data.token;
+				// 	// 将令牌保存到本地存储以备日后使用。
+				// 	localStorage.setItem('token', token);
+				// 	// 重定向至 "/chat" 页面
+				// 	this.$router.push('/chat');
+				// }
+				console.log(response.data);
+			} catch (error) {
+				// Display an error message.
+				console.error(error);
+			}
+		},
+	}
+}
 </script>
