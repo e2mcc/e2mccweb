e310665e9ab287dd1a533a302f6134dda87f8ce4
e2mcc <e2mcc@foxmail.com>
2023-05-22 21:19:18 +0800

    更新了chatgpt包

--- client/service/chatgpt.ts
+++ client/service/chatgpt.ts
@@ -8,7 +8,14 @@ const apiKey = ''
 /**
  * More Info: https://github.com/transitive-bullshit/chatgpt-api
  */
-const api = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY || apiKey })
+const api = new ChatGPTAPI({ 
+	apiKey: process.env.OPENAI_API_KEY || apiKey,
+	completionParams: {
+		model: 'gpt-3.5-turbo',
+		temperature: 0.7,
+		top_p: 0.8
+	}
+})
 
 async function chatReply(message: string) {
   if (!message)
